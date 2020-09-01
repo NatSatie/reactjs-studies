7d7837569596dd03ae760831c3a1bc8b8271785f
Nat <naty.odashima@gmail.com>
2020-09-01 17:35:22 -0300

    have no idea what im doing, testing some sql stuff

---
+++ drinksDatabase.sql
@@ -0,0 +1,6 @@
+INSERT INTO drinks(name, expresso, milk)
+	VALUES ('expresso', 1 , 0);
+INSERT INTO drinks(name, expresso, milk)
+	VALUES ('doppio', 2 , 0);
+INSERT INTO drinks(name, expresso, milk)
+	VALUES ('latte', 1 , 2);
