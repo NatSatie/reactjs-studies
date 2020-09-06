9c343bacba74b4c6de22da0fca632f9ff875279f
Nat <naty.odashima@gmail.com>
2020-09-06 15:45:28 -0300

    remove SQL idea

--- drinksDatabase.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO drinks(name, expresso, milk)
-	VALUES ('expresso', 1 , 0);
-INSERT INTO drinks(name, expresso, milk)
-	VALUES ('doppio', 2 , 0);
-INSERT INTO drinks(name, expresso, milk)
-	VALUES ('latte', 1 , 2);
